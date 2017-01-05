62834270f8742a0fdb0f6cf04ec21ffa7badda59
Sotiri Komissopoulos <sotiri@wireless-s0-no-150-10-125.uchicago.edu>
2017-01-05 16:22:45 -0600

    Finished, I think. I hope the method I used is the intended one.

--- src/java/uchidb/HW0Runner.java
+++ src/java/uchidb/HW0Runner.java
@@ -1,22 +1,79 @@
 package uchidb;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * @author aelmore
  */
 public class HW0Runner {
+    private static Containers<Integer, String> containers = new IntegerStringContainers();
+    
+    // This class is a factory for a singleton containers class.
+    // https://www.tutorialspoint.com/java/java_using_singleton.htm
+    public static Containers<Integer, String> getContainers() {
+        return containers;
+    }
 
-	//TODO you likely will need to add member variable
-	
-	// This class is a factory for a singleton containers class.
-	// https://www.tutorialspoint.com/java/java_using_singleton.htm
-	public static Containers<Integer, String> getContainers() {
-		// TODO fix this function
-		return null;
-	}
+    public static void main(String[] args) {
 
+    }
+}
 
-	public static void main(String[] args){
-		
-	}
+class IntegerStringContainers implements Containers<Integer, String> {
+    private Map<String, Integer> map;
+
+    public IntegerStringContainers() {
+        map = null;
+    }
+
+    // Create a set that stores the array of Integers
+    public Set<Integer> initSet(Integer[] tArray) {
+        return new HashSet<Integer>(Arrays.asList(tArray));
+    }
+    
+    // Create a list that stores an the array of Integers
+    public List<Integer> initList(Integer[] tArray) {
+        return Arrays.asList(tArray);
+    }
+    
+    // Create an empty map that will use Strings as keys and Integers as values
+    public Map<String, Integer> initEmptyMap() {
+        return new HashMap<String, Integer>();
+    }
+    
+    // Store the map in a local field variable -- often called a setter 
+    public void storeMap(Map<String, Integer> mapToStoreInClass) {
+        map = mapToStoreInClass;
+    }
+    
+    // Add a key value to store map with a boolean indicating whether to overwrite existing value
+    public boolean addToMap(String key, Integer value, boolean overwriteExistingKey) {
+        if (overwriteExistingKey || !map.containsKey(key)) {
+            map.put(key, value);
+            return true;
+        }
+        return false;
+    }
+    
+    // Get a value based on a key
+    public Integer getValueFromMap(String key) {
+        if (map.containsKey(key)) {
+            return map.get(key);
+        }
+        return null;
+    }
+    
+    // An overloaded function to get value from map but with a default value
+    // if the key is not present
+    public Integer getValueFromMap(String key, Integer defaultValue) {
+        if (map.containsKey(key)) {
+            return map.get(key);
+        }
+        return defaultValue;
+    }
 }
