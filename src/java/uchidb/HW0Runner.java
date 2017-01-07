2089e6efe47b5d3ce236f9a7e64095102a664f2e
Sotiri Komissopoulos <skomissopoulos@gmail.com>
2017-01-07 16:18:19 -0600

    Made SContainers a generic implementation of Containers.

--- src/java/uchidb/HW0Runner.java
+++ src/java/uchidb/HW0Runner.java
@@ -11,47 +11,49 @@ import java.util.Set;
  * @author aelmore
  */
 public class HW0Runner {
-    private static Containers<Integer, String> containers = new IntegerStringContainers();
+    private static Containers<Integer, String> c = new SContainers<>();
     
     // This class is a factory for a singleton containers class.
     // https://www.tutorialspoint.com/java/java_using_singleton.htm
     public static Containers<Integer, String> getContainers() {
-        return containers;
+        return c;
     }
 
     public static void main(String[] args) {
-
+        
     }
 }
 
-class IntegerStringContainers implements Containers<Integer, String> {
-    private Map<String, Integer> map = null;
-
-    public IntegerStringContainers() { }
+class SContainers<T, S> implements Containers<T, S> {
+    private Map<S, T> map = null;
 
     // Create a set that stores the array of Integers
-    public Set<Integer> initSet(Integer[] tArray) {
-        return new HashSet<Integer>(Arrays.asList(tArray));
+    public Set<T> initSet(T[] tArray) {
+        return new HashSet<T>(Arrays.asList(tArray));
     }
     
     // Create a list that stores an the array of Integers
-    public List<Integer> initList(Integer[] tArray) {
+    public List<T> initList(T[] tArray) {
         return Arrays.asList(tArray);
     }
     
     // Create an empty map that will use Strings as keys and Integers as values
-    public Map<String, Integer> initEmptyMap() {
-        return new HashMap<String, Integer>();
+    public Map<S, T> initEmptyMap() {
+        return new HashMap<S, T>();
     }
     
     // Store the map in a local field variable -- often called a setter 
-    public void storeMap(Map<String, Integer> mapToStoreInClass) {
+    public void storeMap(Map<S, T> mapToStoreInClass) {
         map = mapToStoreInClass;
     }
     
     // Add a key value to store map with a boolean indicating whether to overwrite existing value.
     // Return whether the value was set (not necessarily changed).
-    public boolean addToMap(String key, Integer value, boolean overwriteExistingKey) {
+    public boolean addToMap(S key, T value, boolean overwriteExistingKey) {
+        if (map == null) {
+            return false;
+        }
+
         if (overwriteExistingKey || !map.containsKey(key)) {
             map.put(key, value);
             return true;
@@ -60,13 +62,21 @@ class IntegerStringContainers implements Containers<Integer, String> {
     }
     
     // Get a value based on a key
-    public Integer getValueFromMap(String key) {
+    public T getValueFromMap(S key) {
+        if (map == null) {
+            return null;
+        }
+
         return map.get(key);
     }
     
     // An overloaded function to get value from map but with a default value
     // if the key is not present
-    public Integer getValueFromMap(String key, Integer defaultValue) {
+    public T getValueFromMap(S key, T defaultValue) {
+        if (map == null) {
+            return null;
+        }
+        
         if (map.containsKey(key)) {
             return map.get(key);
         }
